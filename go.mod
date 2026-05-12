ad174ae0b6931bf7614b8683a08eda3b480a6e6f
Sania220107 <ristasania22@gmail.com>
2026-05-12 14:58:47 +0700

    setup awal project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cine-review
+
+go 1.25.1
---
+++ model.go
@@ -0,0 +1,11 @@
+package main
+
+type Film struct {
+	judul      string
+	genre      string
+	tahun      int
+	rating     float64
+	deskripsi  string
+}
+
+var daftarFilm []Film
