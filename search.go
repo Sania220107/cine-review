c7834ae84e8fcad33dd503e279a05a3bbe823586
Damaoooux <yusufhibatullah98@gmail.com>
2026-05-14 22:39:26 +0700

    statistik dan search

--- search.go
+++ search.go
@@ -1,1 +1,48 @@
 package main
+
+func sequentialSearchJudul(judul string) int {
+	for i, film := range daftarFilm {
+		if film.judul == judul {
+			return i
+		}
+	}
+	return -1
+}
+
+func binarySearchJudul(judul string) int {
+	if len(daftarFilm) == 0 {
+		return -1
+	}
+
+	type idxJudul struct {
+		idx   int
+		judul string
+	}
+	temp := make([]idxJudul, len(daftarFilm))
+	for i, f := range daftarFilm {
+		temp[i] = idxJudul{idx: i, judul: f.judul}
+	}
+
+	for i := 1; i < len(temp); i++ {
+		key := temp[i]
+		j := i - 1
+		for j >= 0 && temp[j].judul > key.judul {
+			temp[j+1] = temp[j]
+			j--
+		}
+		temp[j+1] = key
+	}
+
+	low, high := 0, len(temp)-1
+	for low <= high {
+		mid := (low + high) / 2
+		if temp[mid].judul == judul {
+			return temp[mid].idx
+		} else if temp[mid].judul < judul {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
--- statistik.go
+++ statistik.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+)
+
+func statistikFilm() {
+	if len(daftarFilm) == 0 {
+		fmt.Println("Tidak ada data film.")
+		return
+	}
+
+	genreCount := make(map[string]int)
+	totalRating := 0.0
+
+	for _, film := range daftarFilm {
+		genreCount[film.genre]++
+		totalRating += film.rating
+	}
+
+	fmt.Println("\n=== STATISTIK FILM ===")
+	fmt.Println("Jumlah film per genre:")
+	for genre, count := range genreCount {
+		fmt.Printf("  %s : %d film\n", genre, count)
+	}
+
+	averageRating := totalRating / float64(len(daftarFilm))
+	fmt.Printf("\nRata-rata rating seluruh koleksi: %.2f\n", averageRating)
+}
